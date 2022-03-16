c554e1b7d01d834131a1388175c04f7795f59a9b
Maksim Pantelejev <vertumik@gmail.com>
2022-03-16 17:34:30 +0200

    done html cars

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -2,13 +2,13 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import web.service.CarService;
 
 @Controller
 public class CarsController {
 
-    @RequestMapping("/cars")
+    @GetMapping("/cars")
     public String getCars(ModelMap model){
         model.addAttribute("cars", new CarService().getCars());
         return "cars";
